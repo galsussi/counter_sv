fcd8e2bb3a5f8c80ade7ec6283abdbad62f8cc08
Gal Sussi <GalSussi9@gmail.com>
2025-08-11 16:45:27 +0300

    Remove files outside rtl and dv directories

--- counter_inf.svh
+++
@@ -1,6 +0,0 @@
-interface counter_inf #(parameter DATA_WIDTH =8)(logic clk,rstN);
-    
-    logic enable , load;
-    logic [DATA_WIDTH-1: 0] Data_in ,count;
-    modport DUT (input enable,load,Data_in , clk,rstN, output count);
-endinterface
