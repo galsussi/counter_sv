981a13dbbcc25f0608b912f63c396727567e07e5
Gal Sussi <GalSussi9@gmail.com>
2025-08-11 18:44:10 +0300

    add Driver class

--- rtl/counter.svh
+++ rtl/counter.svh
@@ -5,9 +5,6 @@ module counter(counter_inf.DUT inf_i
 
 always @(posedge inf_i.clk, negedge inf_i.rstN) begin
     if(!inf_i.rstN) begin
-    inf_i.enable<=1'b0;
-    inf_i.load<=1'b0;
-    inf_i.Data_in<=inf_i.DATA_WIDTH'h00;
     inf_i.count <=inf.DATA_WIDTH'h00;
     end
 else if(inf_i.load && inf_i.enable)
